e00f34df74911dfbf39d35c03c8e338854f735f5
Joe Thor <jfthor@umn.edu>
2018-05-18 13:32:01 -0500

    reads the url csv from the test file, prints out all the csv data

--- webscraper.py
+++ webscraper.py
@@ -1,9 +1,24 @@
 import csv
 import urllib2
 
+file_1 = open("ListOfURLStest.txt", "r+") 
+file_2= open("Results.txt", "r+")
+
 #https://stackoverflow.com/questions/16283799/how-to-read-a-csv-file-from-a-url-with-python
 
 #I could create a text file that can be read in as a list (or dictionary with URL : CR#) which prints its findings puts it in another file based on the count
+
+urlArray = file_1.readlines()
+
+for urlcsv in urlArray:
+	response = urllib2.urlopen(urlcsv)
+	cr = csv.reader(response)
+	
+	for row in cr:
+		print row
+		
+
+'''
 
 url = 'https://z.umn.edu/shortener/urls/154734/csv/click_data.csv' 
 response = urllib2.urlopen(url)
@@ -15,6 +30,7 @@ for row in cr:
     count = count + 1
 	
 print count
+'''
 
 
 #https://z.umn.edu/shortener/urls/154734/csv/click_data.csv URL for CR13 before clicked the export to CSV button, the screen defaults in on "last 24 hours" which is 0 currently and aftering
