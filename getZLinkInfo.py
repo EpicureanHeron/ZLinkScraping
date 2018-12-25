86414d1b01241807026d7d507ae4f0568bd65562
Joe Thor <joethor1989@gmail.com>
2018-12-25 15:55:32 -0600

    added some notes on speeding up the processing time and added a log

--- getZLinkInfo.py
+++ getZLinkInfo.py
@@ -13,6 +13,9 @@ def ZData():
 	
 	file_1 = open("ListOfURLs.txt", "r+") 
 	file_2= open(name, "w")
+	# not sure if this part is working, tested in Ames and...internet is not good.
+	# the 'a' should append the data at the end of the file
+	log = open("log.txt", "a")
 
 	urlArray = file_1.readlines()
 
@@ -21,6 +24,9 @@ def ZData():
 	file_2.write("The data in the file was created at " + str(now)+ '\n')
 	print "Processing..."
 	for urlcsv in urlArray:
+		# to cut down on processing time I should try to just read how many rows, rather than clicking through each row (which is very ineffecient)
+		# SO link https://stackoverflow.com/questions/16108526/count-how-many-lines-are-in-a-csv-python
+
 		try: 
 			response = urllib2.urlopen(urlcsv)
 			cr = csv.reader(response)
@@ -33,7 +39,9 @@ def ZData():
 		except: 
 			print urlcsv + " failed! "
 	end = time.time()
-	print (end - start) 
+	totalTime = (end-start)
+	# hopefully appends the run time on the log, just so we are tracking it. Would like to get a current baseline in there.
+	log.append(str(now) + ": " + str(totalTime) + '\n') 
 	print "Success! Check the " + name + " file."
 
 
