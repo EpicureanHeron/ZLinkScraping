a899a84f363ae9efd245f9fb2806d066759f245f
Joe Thor <joethor1989@gmail.com>
2019-01-04 07:12:41 -0600

    decoding the url response is working, but not passable to the csv counter

--- test.py
+++ test.py
@@ -9,17 +9,26 @@ test = 'http://z.umn.edu/shortener/urls/154723/csv/click_data.csv'
 request = urllib.request.Request(test)
 print(request)
 response = urllib.request.urlopen(request)
-print(response)
+#print(response)
 # reads csv results but not as a csv????? Seems to be the raw response including \n for new lines and all.
 # I don't think the response is the csv...so how do i get that ?
-renderedResponse = response.read()
-print(renderedResponse)
+# had to decode the renderped response (not sure if 'ASCII' or "utf-8"), however, when I run the decoded data through the below
+# counter, it displays as a 5 digit number rather than the expected 77-78. 
+# could be the delimiter...or the decoding, not sure which
+# similair issue reported here: https://stackoverflow.com/questions/18897029/read-csv-file-from-url-into-python-3-x-csv-error-iterator-should-return-str
+
+
+renderedResponse = response.read().decode('ASCII')
+
+
+#print(renderedResponse)
 
 ## Reads local file and counts the lines 
 # MUCH more efficient that previous 
-with open('click_data.csv', 'r') as csvfile:
+
+with open(renderedResponse, 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
     row_count = sum(1 for row in spamreader)
     
-    print(row_count)
+    print(row_count - 1)
         
